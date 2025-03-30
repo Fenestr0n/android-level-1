c96125462490b765ee9ed17e922b7680348b3c9a
Roman Kozhin <kozhin.rm@mail.ru>
2025-03-30 08:45:15 +0300

    5.3 Методы ЖЦ onStart и onStop

--- GameScore/app/src/main/java/ru/kivart/gamescore/MainActivity.java
+++ GameScore/app/src/main/java/ru/kivart/gamescore/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.kivart.gamescore;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,10 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private int score1 = 0;
     private int score2 = 0;
+    private TextView textViewScore1;
+    private TextView textViewScore2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "onCreate");
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -27,43 +31,69 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        textViewScore1 = findViewById(R.id.textViewScore1);
+        textViewScore2 = findViewById(R.id.textViewScore2);
+
 //        При первом создании activity еще нет сохраненного состояния
         if (savedInstanceState != null) {
             score1 = savedInstanceState.getInt("score1");
             score2 = savedInstanceState.getInt("score2");
         }
 
-        TextView textViewScore1 = findViewById(R.id.textViewScore1);
-        TextView textViewScore2 = findViewById(R.id.textViewScore2);
-
-        textViewScore1.setText(String.valueOf(score1));
-        textViewScore2.setText(String.valueOf(score2));
+        updateScore1();
+        updateScore2();
 
         textViewScore1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewScore1.setText(String.valueOf(++score1));
+                Log.d("MainActivity", "Clicked textViewScore1");
+                score1++;
+                updateScore1();
             }
         });
 
         textViewScore2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewScore2.setText(String.valueOf(++score2));
+                Log.d("MainActivity", "Clicked textViewScore2");
+                score2++;
+                updateScore2();
             }
         });
 
     }
 
+    private void updateScore1() {
+        textViewScore1.setText(String.valueOf(score1));
+    }
+
+    private void updateScore2() {
+        textViewScore2.setText(String.valueOf(score2));
+    }
+
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        Log.d("MainActivity", "onSaveInstanceState");
         super.onSaveInstanceState(outState);
         outState.putInt("score1", score1);
         outState.putInt("score2", score2);
     }
 
     @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop");
+        super.onStop();
+    }
+
+    @Override
     protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy");
         super.onDestroy();
     }
 }
